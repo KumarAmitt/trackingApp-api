c731e27c62d6987356f888e0fd236e819fceaa1a
KumarAmitt <amitt2015@outlook.com>
2021-08-18 22:22:02 +0530

    Fix error in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params['user']['username']).try(:authenticate, params['user']['password'])
+    user = User.find_by(username: params['user']['username']).try(:authenticate, params['user']['password'])
 
     if user
       session[:user_id] = user.id
