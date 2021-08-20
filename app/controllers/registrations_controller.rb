ad45c6751d7de11e7f1478985d734f92c27f4cca
KumarAmitt <amitt2015@outlook.com>
2021-08-20 22:07:08 +0530

    Declare privaate method user_params in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,9 @@
 class RegistrationsController < ApplicationController
   def create
-    user = User.create!(username: params['user']['username'], password: params['user']['password'],
-                        password_confirmation: params['user']['password_confirmation'])
+    # user = User.create!(username: params['user']['username'], password: params['user']['password'],
+    #                     password_confirmation: params['user']['password_confirmation'])
+
+    user = User.create!(user_params)
 
     if user
       session[:user_id] = user.id
@@ -9,5 +11,11 @@ class RegistrationsController < ApplicationController
     else
       render json: { status: 500 }
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true, length: { in: 4..8 }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
