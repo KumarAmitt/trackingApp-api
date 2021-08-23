a4a35e895c9b63c62b53b7af8650f8ad371ae9c6
KumarAmitt <amitt2015@outlook.com>
2021-08-23 20:04:22 +0530

    Add deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,2 +1,39 @@
 class DealsController < ApplicationController
+  include CurrentUserConcern
+
+  def index
+    deals = @current_user.deals.includes(:product).order(created_at: :desc)
+    data = Hash.new { |k, v| k[v] = [] }
+    deals.each do |e|
+      data[e.product.product_name] << e
+    end
+
+    render json: { data: data, status: :ok }
+  end
+
+  def create
+    deal = @current_user.deals.build(deal_params)
+    deal.product_id = params[:product_id]
+    if deal.save
+      render json: { deal: deal }
+    else
+      render json: { status: 'Invalid data' }
+    end
+  end
+
+  def update
+    deal = Deal.find(params[:id]).update(deal_params)
+    render json: { deal: deal }
+  end
+
+  def destroy
+    deal = Deal.find(params[:id])
+    deal.destroy
+  end
+
+  private
+
+  def deal_params
+    params.permit(:premium, :application_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
     resources :deals, only: [:create, :update, :destroy]
   end
+
+  resources :deals, only: [:index]
 end
