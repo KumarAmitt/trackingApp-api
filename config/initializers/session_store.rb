850d2779ac382a9e43f71e81130f757342f315b2
KumarAmitt <amitt2015@outlook.com>
2021-08-26 23:11:49 +0530

    Add active record session gem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,11 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_deals_tracker_app', domain: 'deals-tracker.herokuapp.com'
-else
-  Rails.application.config.session_store :cookie_store, key: '_deals_tracker_app'
-end
+# if Rails.env == 'production'
+#   Rails.application.config.session_store :cookie_store, key: '_deals_tracker', domain: 'deals-tracker.herokuapp.com'
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_deals_tracker'
+# end
 
-# Rails.application.config.session_store :cookie_store, key: '_deals_tracker_app', domain: :all, tld_length: 2
+# Rails.application.config.session_store :cookie_store, key: '_deals_tracker', domain: :all
+
+Rails.application.config.session_store :active_record_store, :key => '_deals_tracker'
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_092436) do
+ActiveRecord::Schema.define(version: 2021_08_26_173830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2021_08_23_092436) do
     t.string "product_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
