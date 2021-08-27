c8ea866a2fbc15c336fea82d9da7940477d9640d
KumarAmitt <amitt2015@outlook.com>
2021-08-27 23:17:47 +0530

    Revert cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'http://localhost:3000'
-  #   resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
-  # end
-
-  # allow do
-  #   origins 'https://deals-tracker.herokuapp.com'
-  #   resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
-  # end
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+  end
 
   allow do
-    origins '*'
+    origins 'https://deals-tracker.herokuapp.com'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
