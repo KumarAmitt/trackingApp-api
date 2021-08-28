7bb0e1212609ff8d09bc2e06aa7a40bdcf1f8120
KumarAmitt <amitt2015@outlook.com>
2021-08-28 13:33:18 +0530

    Change url in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://deals-tracker.herokuapp.com'
+    origins 'https://tracking-app-frontend.herokuapp.com/'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
