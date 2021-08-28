163a64ca93111f6ff4b7ff70d90d3f75aebfcbb1
KumarAmitt <amitt2015@outlook.com>
2021-08-28 21:09:49 +0530

    Add validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :deals, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true, length: { in: 4..8 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..8 }
   validates :password, presence: true
   validates :password_confirmation, presence: true
 end
