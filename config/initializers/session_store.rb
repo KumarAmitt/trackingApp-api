dc4db2ae6b8f627ebbda1b3f796dfba706fc33a8
KumarAmitt <amitt2015@outlook.com>
2021-09-04 02:27:28 +0530

    Update session_store configuration for production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_deals_tracker', domain: 'deals-tracker.herokuapp.com', tld_length: 3
+  Rails.application.config.session_store :cookie_store, key: '_deals_tracker', domain: 'deals-tracker.herokuapp.com', :same_site => :none, :secure => :true, tld_length: 3
 else
   Rails.application.config.session_store :cookie_store, key: '_deals_tracker'
 end
